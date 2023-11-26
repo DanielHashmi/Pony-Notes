c3757dc0118b228b6a1e2eb3577caeca822b310c
Daniel Hashmi <danialhashmi418@gmail.com>
2023-11-26 08:03:46 +0500

    Added Local Storage & fixed the line through bug when clicked

--- main.js
+++ main.js
@@ -1,24 +1,69 @@
+// Declaring main variables
 let inp = document.getElementsByClassName('inp')[0];
 let pencil = document.getElementsByClassName('fas')[0];
 let adds = document.getElementsByClassName('adds')[0];
+let h2 = document.getElementsByTagName('h2')
 
-let emp = "";
+// Declaring main Function;
+function main() {
+    let array = JSON.parse(localStorage.getItem("name")) ?? [];
+    let obj = {
+        name: inp.value
+    };
+    array.push(obj);
+    localStorage.setItem("name", JSON.stringify(array));
 
+    let h2 = document.createElement("h2");
+    array.forEach(e => {
+        h2.innerHTML = e.name;
+        adds.appendChild(h2);
+    });
+
+    // Adding lineThrough Function to the h2 elements inside the main function
+    h2.addEventListener('click', () => {
+        if (h2.style.textDecoration != 'line-through') {
+            h2.style.textDecoration = 'line-through'
+        } else {
+            h2.style.textDecoration = 'none'
+        }
+    })
+}
+
+// Declaring a function for the local storage to show everything on the page;
+function local() {
+    let array = JSON.parse(localStorage.getItem("name")) ?? [];
+    array.forEach(e => {
+        adds.innerHTML += `<h2>${e.name}</h2>`;
+    });
+};
+
+// Calling the local function;
+local();
+
+// Calling the main function inside this event;
 inp.addEventListener('keydown', (e) => {
     if (e.key == "Enter") {
-        let cre = document.createElement('h2');
-        cre.innerHTML = inp.value
-        console.log(cre)
-        adds.insertAdjacentElement("beforeend", cre)
-        inp.value = ""
-        cre.addEventListener('click', () => {
-            cre.style.textDecoration = 'line-through'
+        main();
+        inp.value = "";
+    };
+});
+
+// Clearing the local storage with this event;
+pencil.addEventListener('click', () => {
+    localStorage.clear();
+    adds.innerHTML = '';
+});
+
+// Adding lineThrough Function to the h2 elements in the global scope
+lineThrough()
+function lineThrough() {
+    Array.from(h2).forEach((e) => {
+        e.addEventListener('click', () => {
+            if (e.style.textDecoration != 'line-through') {
+                e.style.textDecoration = 'line-through'
+            } else {
+                e.style.textDecoration = 'none'
+            }
         })
-        cre.addEventListener('dblclick', () => {
-            cre.style.textDecoration = 'none'
-        })
-        pencil.addEventListener('click', () => {
-            adds.innerHTML = ""
-        })
-    }
-})
+    })
+}
